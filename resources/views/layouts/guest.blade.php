a52adc9d244338d83edc58d944262a893aeb72de
exeeny <your-email@example.com>
2025-04-15 13:17:07 +0400

    added modes at guest layout

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,5 +1,9 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}"
+x-data="{ isDarkMode: localStorage.getItem('dark')==='true'}" 
+x-bind:class="{ 'dark': isDarkMode }"
+x-init="$watch('isDarkMode', val => localStorage.setItem('dark', val))"
+>
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -15,15 +19,24 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans text-gray-900 antialiased">
+    <nav class="fixed top-0 right-0 m-2">
+    <x-modes/>
+    
+    </nav>
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
             <div>
+            
                 <a href="/">
                     <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
                 </a>
             </div>
 
             <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
+            
                 {{ $slot }}
+                
+               
+                
             </div>
         </div>
     </body>
